23cdb5790df5e0e9479548adaa7c8f64ed908815
AwpData <awpdata@gmail.com>
2019-01-09 10:56:13 -0800

    Updated for license plate formatting

--- Vehicle.java
+++ Vehicle.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
+
 public class Vehicle {
 	private String VehicleName;
-	private int speed;
+	protected int speed;
 	private String status = "stopped";
-	public Vehicle(String VehicleName) {
+	private String LicensePlate;
+
+	public Vehicle(String VehicleName, String LicensePlate) {
 		this.VehicleName = VehicleName;
+		this.LicensePlate = LicensePlate;
 	}
+
 	public String getVehicleStatus() {
+		System.out.println(VehicleName + " license plate: " + LicensePlate);
 		System.out.println(VehicleName + " is currently " + status);
 		System.out.println(VehicleName + " is going " + speed + " MPH");
 		return "";
 	}
+
 	public String start() {
 		if (status.equals("started")) {
 			System.out.println("Vehicle has already been started");
@@ -19,6 +26,7 @@ public class Vehicle {
 		}
 		return status = "started";
 	}
+
 	public String stop() {
 		if (status.equals("stopped")) {
 			System.out.println("Vehicle has already been stopped");
@@ -27,19 +35,24 @@ public class Vehicle {
 		}
 		return status = "stopped";
 	}
+
 	public String turn() {
 		return "turning...";
 	}
+
 	public int setSpeed(int num) {
 		speed = num;
 		return speed;
 	}
+
 	public String getSpeed() {
 		return speed + " MPH";
 	}
+
 	public int increaseSpeed(int num) {
 		return speed += num;
 	}
+
 	public int decreaseSpeed(int num) {
 		speed -= num;
 		if (speed < 0) {
@@ -47,18 +60,22 @@ public class Vehicle {
 		}
 		return speed;
 	}
+
 	public static int getinput() {
 		Scanner input = new Scanner(System.in);
 		int num = input.nextInt();
 		return num;
 	}
+
 	public String getVehicleName() {
 		return VehicleName;
 	}
+
 	public String getVehicleOnOrOff() {
 		return status;
 	}
-	public int getVehicleSpeed() {
-		return speed;
+
+	public String getLicensePlate() {
+		return LicensePlate;
 	}
 }
