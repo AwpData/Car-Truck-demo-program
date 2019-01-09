ad0839edb97a0eb686f1bd9f939997f77ece0a5a
AwpData <awpdata@gmail.com>
2019-01-09 10:55:20 -0800

    Added max speed, unique license plate

--- Car.java
+++ Car.java
@@ -1,15 +1,36 @@
+import java.util.Random;
+
 public class Car extends Vehicle {
+	static Random num = new Random();
 	private String trunk = "Closed";
 
 	public Car(String VehicleName) {
-		super(VehicleName);
+		super(VehicleName, "C" + num.nextInt(1000));
+	}
+
+	@Override
+	public int setSpeed(int num) {
+		if (num > 300) {
+			System.out.println("Error, speed over 300, setting speed to 300.");
+			super.speed = 300;
+		} else {
+			super.speed = num;
+		}
+		return super.speed = speed;
+	}
+
+	public void checkSpeed() {
+		if (super.speed == 300 || super.speed > 300) {
+			System.out.println("Error, speed over 300, setting speed to 300.");
+			super.speed = 300;
+		}
 	}
 
 	public String openTrunk() {
 		if (trunk.equals("open")) {
 			System.out.println("Trunk is already open cannot open again");
 		} else {
-			System.out.println("Opening Trunk on " + this.getVehicleName() + "...");
+			System.out.println("Opening Trunk on " + getVehicleName() + "...");
 		}
 		return trunk = "open";
 	}
@@ -18,14 +39,16 @@ public class Car extends Vehicle {
 		if (trunk.equals("closed")) {
 			System.out.println("Trunk is already closed cannot close again");
 		} else {
-			System.out.println("Closing Trunk on " + this.getVehicleName() + "...");
+			System.out.println("Closing Trunk on " + getVehicleName() + "...");
 		}
 		return trunk = "closed";
 	}
 
+	@Override
 	public String getVehicleStatus() {
-		System.out.println(this.getVehicleName() + " is currently " + this.getVehicleOnOrOff());
-		System.out.println(this.getVehicleName() + " is going " + this.getSpeed());
+		System.out.println(getVehicleName() + " license plate: " + getLicensePlate());
+		System.out.println(getVehicleName() + " is currently " + getVehicleOnOrOff());
+		System.out.println(getVehicleName() + " is going " + getSpeed());
 		System.out.println("Trunk is " + trunk);
 		return "";
 	}
