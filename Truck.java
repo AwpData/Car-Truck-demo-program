96ce0681b5d5b2df6a212f89a49d4bfbba6bcfa5
AwpData <awpdata@gmail.com>
2019-01-09 10:55:45 -0800

    Added max speed, unique license plate

--- Truck.java
+++ Truck.java
@@ -1,15 +1,36 @@
+import java.util.Random;
+
 public class Truck extends Vehicle {
+	static Random num = new Random();
 	private String tailgate = "closed";
 
 	public Truck(String VehicleName) {
-		super(VehicleName);
+		super(VehicleName, "T" + num.nextInt(1000));
+	}
+
+	@Override
+	public int setSpeed(int num) {
+		if (num > 75) {
+			System.out.println("Error, speed over 75, setting speed to 75.");
+			super.speed = 75;
+		} else {
+			super.speed = num;
+		}
+		return super.speed = speed;
+	}
+
+	public void checkSpeed() {
+		if (super.speed == 75 || super.speed > 75) {
+			System.out.println("Error, speed over 75, setting speed to 75.");
+			super.speed = 75;
+		}
 	}
 
 	public String openTailgate() {
 		if (tailgate.equals("open")) {
 			System.out.println("Trunk is already open cannot open again");
 		} else {
-			System.out.println("Opening Tailgate on " + this.getVehicleName() + "...");
+			System.out.println("Opening Tailgate on " + getVehicleName() + "...");
 		}
 		return tailgate = "open";
 	}
@@ -18,14 +39,16 @@ public class Truck extends Vehicle {
 		if (tailgate.equals("closed")) {
 			System.out.println("Trunk is already closed cannot close again");
 		} else {
-			System.out.println("Closing Tailgate on " + this.getVehicleName() + "...");
+			System.out.println("Closing Tailgate on " + getVehicleName() + "...");
 		}
 		return tailgate = "closed";
 	}
 
+	@Override
 	public String getVehicleStatus() {
-		System.out.println(this.getVehicleName() + " is currently " + this.getVehicleOnOrOff());
-		System.out.println(this.getVehicleName() + " is going " + this.getSpeed());
+		System.out.println(getVehicleName() + " license plate: " + getLicensePlate());
+		System.out.println(getVehicleName() + " is currently " + getVehicleOnOrOff());
+		System.out.println(getVehicleName() + " is going " + getSpeed());
 		System.out.println("Tailgate is " + tailgate);
 		return "";
 	}
