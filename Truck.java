d0622e72470d1a1995ca16e90a6e5d1b74dec02b
AwpData <awpdata@gmail.com>
2019-01-10 09:41:16 -0800

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -10,11 +10,20 @@ public class Truck extends Vehicle {
 
 	@Override
 	public int setSpeed(int num) {
-		if (num > 75) {
-			System.out.println("Error, speed over 75, setting speed to 75.");
-			super.speed = 75;
+		if (getVehicleOnOrOff().equals("stopped")) {
+			System.out.println("Start the vehicle first!");
 		} else {
 			super.speed = num;
+			if (num > 75) {
+				System.out.println("Error, speed over 75, setting speed to 75.");
+				super.speed = 75;
+			} else {
+				super.speed = num;
+			}
+			if (super.speed == 0) {
+				System.out.println("Speed is 0, stopping car");
+				super.setVehicleStatusOff();
+			}
 		}
 		return super.speed = speed;
 	}
@@ -24,24 +33,38 @@ public class Truck extends Vehicle {
 			System.out.println("Error, speed over 75, setting speed to 75.");
 			super.speed = 75;
 		}
+		if (super.speed == 0 && super.getVehicleOnOrOff().equals("started")) {
+			System.out.println("Speed decreased to 0, vehicle is stopping");
+			super.setVehicleStatusOff();
+		}
 	}
 
 	public String openTailgate() {
-		if (tailgate.equals("open")) {
-			System.out.println("Trunk is already open cannot open again");
+		if (getVehicleOnOrOff().equals("started")) {
+			System.out.println("Stop the vehicle first!");
+			return tailgate;
 		} else {
-			System.out.println("Opening Tailgate on " + getVehicleName() + "...");
+			if (tailgate.equals("open")) {
+				System.out.println("Trunk is already open cannot open again");
+			} else {
+				System.out.println("Opening Tailgate on " + getVehicleName() + "...");
+			}
+			return tailgate = "open";
 		}
-		return tailgate = "open";
 	}
 
 	public String closeTailgate() {
-		if (tailgate.equals("closed")) {
-			System.out.println("Trunk is already closed cannot close again");
+		if (getVehicleOnOrOff().equals("started")) {
+			System.out.println("Stop the vehicle first!");
+			return tailgate;
 		} else {
-			System.out.println("Closing Tailgate on " + getVehicleName() + "...");
+			if (tailgate.equals("closed")) {
+				System.out.println("Trunk is already closed cannot close again");
+			} else {
+				System.out.println("Closing Tailgate on " + getVehicleName() + "...");
+			}
+			return tailgate = "closed";
 		}
-		return tailgate = "closed";
 	}
 
 	@Override
