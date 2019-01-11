83c8c9036bd3738636a6566e8451b95a78bb7cf2
AwpData <awpdata@gmail.com>
2019-01-11 09:45:31 -0800

    Added Vehicle Information

--- Driver.java
+++ Driver.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-abstract public class Driver {
+public class Driver {
 	public static void main(String[] args) {
 		int num, carturns = 0, truckturns = 0;
 		String choice = "hi";
 		Scanner input = new Scanner(System.in);
 		Car car = new Car("car"); // CHANGE TEXT IN QUOTATIONS
 		Truck truck = new Truck("truck"); // CHANGE TEXT IN QUOTATION
-		while (!choice.equals("13")) {
+		while (!choice.equals("14")) {
 			System.out.println("What do you want to do?");
 			System.out.println("1. Vehicle status");
 			System.out.println("2. Start Vehicle  /  3. Stop Vehicle");
@@ -16,7 +16,8 @@ abstract public class Driver {
 			System.out.println("7. Increase Speed  /  8. Decrease Speed");
 			System.out.println("9. Open Trunk  /  10. Close Trunk");
 			System.out.println("11. Open Tailgate  /  12. Close Tailgate");
-			System.out.println("13. QUIT");
+			System.out.println("13. Info on each vehicle");
+			System.out.println("14. QUIT");
 			choice = input.nextLine();
 			System.out.println();
 			if (choice.equals("1")) { // Checks all the statistics of the
@@ -128,6 +129,18 @@ abstract public class Driver {
 			}
 			if (choice.equals("12")) {
 				truck.closeTailgate();
+			}
+
+			if (choice.equals("13")) { // Abstract methods
+				System.out.println("Car Info:");
+				car.getGasMileage();
+				car.getMaxSpeed();
+				car.getWeight();
+				System.out.println();
+				System.out.println("Truck Info:");
+				truck.getGasMileage();
+				truck.getMaxSpeed();
+				truck.getWeight();
 			}
 			System.out.println();
 		}
